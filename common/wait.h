35c34d5122e0825baef7497e4eecab838f26db4f
Fred Sundvik <fsundvik@gmail.com>
2016-05-07 15:09:01 +0300

    Add variable wait_ms

--- common/wait.h
+++ common/wait.h
@@ -9,12 +9,19 @@ extern "C" {
 #   include <util/delay.h>
 #   define wait_ms(ms)  _delay_ms(ms)
 #   define wait_us(us)  _delay_us(us)
+static inline void wait_ms_variable(unsigned int ms) {
+    while(ms--) {
+        _delay_ms(1);
+    }
+}
 #elif defined(PROTOCOL_CHIBIOS) /* __AVR__ */
 #   include "ch.h"
 #   define wait_ms(ms) chThdSleepMilliseconds(ms)
 #   define wait_us(us) chThdSleepMicroseconds(us)
+#   define wait_ms_variable(ms) wait_ms(ms)
 #elif defined(__arm__) /* __AVR__ */
 #   include "wait_api.h"
+#   define wait_ms_variable(ms) wait_ms(ms)
 #endif /* __AVR__ */
 
 #ifdef __cplusplus
