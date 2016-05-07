3d5dbb32346ee1d5d571963355bd4db59dd1b8b2
Fred Sundvik <fsundvik@gmail.com>
2016-05-07 11:37:43 +0300

    Update mbed driver to new format

--- protocol/mbed/mbed_driver.cpp
+++ protocol/mbed/mbed_driver.cpp
@@ -12,8 +12,21 @@ static void send_keyboard(report_keyboard_t *report);
 static void send_mouse(report_mouse_t *report);
 static void send_system(uint16_t data);
 static void send_consumer(uint16_t data);
+static void init_driver(void) {}
+static bool is_connected(void) {return keyboard.configured();}
+// The HIDKeyboard does not seem to expose the suspend state
+static bool is_suspended(void) {return false;}
+static void poll_usb(void) {}
+static bool is_remote_wakeup_supported() {return false;}
+static void send_remote_wakeup(void) {}
 
 host_driver_t mbed_driver = {
+    init_driver,
+    is_connected,
+    is_suspended,
+    poll_usb,
+    is_remote_wakeup_supported,
+    send_remote_wakeup,
     keyboard_leds,
     send_keyboard,
     send_mouse,
